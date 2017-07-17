0568d620d8476730cf7e10bd20d8cd781dc0576b
come25136 <come25136@gmail.com>
2017-07-17 22:37:09 +0900

    [add] RT選別機能付けたりハッシュタグに対応させたり

--- private/js/get_tweet.js
+++ private/js/get_tweet.js
@@ -16,8 +16,6 @@ function start(token, token_secret, limit) {
   var w_size = 48;
 
   socketio.on('tweet', function (data) {
-    console.log(data);
-
     $tc = $tweets.children();
 
     if (limit <= $tc.length) {
@@ -27,26 +25,45 @@ function start(token, token_secret, limit) {
     var wh = window.innerHeight - 104;
     var ww = window.innerWidth - 300;
 
-    $tweets.append('<div class="tweet" id="' + data.id_str + '" style="top:' + calculation(wh / 100 * w_size, wh, 104) + 'px; left:' + calculation(ww / 100 * w_size, ww, 300) + 'px; z-index:' + z_count + ';">'
-      + '<div class="title_bar">'
-      + 'OZtter2 Message - <a href="https://twitter.com/' + data.user.screen_name + '" target="_blank">@' + data.user.screen_name
-      + '</a><button class="close" title="閉じる">'
-      + '<span class="cross"></span>'
-      + '</button>'
-      + '</div>'
-      + '<img class="icon" align="left" src="' + data.user.profile_image_url_https + '">'
-      + '<p class="value">' + tweet_value(data.text) + '</p>'
-      + '</div>');
+    if (data.retweeted_status) {
+      data.text = data.text.replace(/RT\s@.+:\s/g, '');
 
-    function calculation(min, max, size) {
-      return Math.random() * ((max - min) - min) + min;
+      if (data.is_quote_status) {
+        add('RT Quote <a href="https://twitter.com/' + data.retweeted_status.user.screen_name + '" target="_blank">@' + data.retweeted_status.user.screen_name + '</a>');
+      } else {
+        add('RT <a href="https://twitter.com/' + data.retweeted_status.user.screen_name + '" target="_blank">@' + data.retweeted_status.user.screen_name + '</a>');
+      }
+    } else if (data.is_quote_status) {
+      add('Quote');
+    } else if (data.in_reply_to_status_id_str) {
+      add('Reply');
+    } else {
+      add('');
     }
 
-    function tweet_value(value) {
-      return value
-        .replace(/(https?:\/\/[\x21-\x7e]+)/gi, '<a href="$1" target="_blank">$1</a>') // メンションのURLと干渉するため、必ず一番最初にこの処理を書くこと
-        .replace(/@(\w+)/g, '<a href="https://twitter.com/$1" target="_blank">@$1</a>')
-        .replace(/\r\n?/g, "<br>");
+    function add(type) {
+      $tweets.append('<div class="tweet" id="' + data.id_str + '" style="top:' + calculation(wh / 100 * w_size, wh, 104) + 'px; left:' + calculation(ww / 100 * w_size, ww, 300) + 'px; z-index:' + z_count + ';">'
+        + '<div class="title_bar">'
+        + 'OZtter2 Message - <a href="https://twitter.com/' + data.user.screen_name + '" target="_blank">@' + data.user.screen_name
+        + '</a> ' + type + '<button class="close" title="閉じる">'
+        + '<span class="cross"></span>'
+        + '</button>'
+        + '</div>'
+        + '<img class="icon" align="left" src="' + data.user.profile_image_url_https + '">'
+        + '<p class="value">' + tweet_value(data.text) + '</p>'
+        + '</div>');
+
+      function calculation(min, max, size) {
+        return Math.random() * ((max - min) - min) + min;
+      }
+
+      function tweet_value(value) {
+        return value
+          .replace(/(https?:\/\/[\x21-\x7e]+)/gi, '<a href="$1" target="_blank">$1</a>') // メンションのURLと干渉するため、必ず一番最初にこの処理を書くこと
+          .replace(/#(w*[一-龠_ぁ-ん_ァ-ヴーａ-ｚＡ-Ｚa-zA-Z0-9]+|[a-zA-Z0-9_]+|[a-zA-Z0-9_]w*)/g, '<a href="https://twitter.com/search?q=%23$1" target="_blank">#$1</a>')
+          .replace(/@(\w+)/g, '<a href="https://twitter.com/$1" target="_blank">@$1</a>')
+          .replace(/\r\n?/g, "<br>");
+      }
     }
 
     $('#' + data.id_str).draggable({ handle: '.title_bar' });
